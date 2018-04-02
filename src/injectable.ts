5c5dfec4648aca9bd42dc976c7167a721273459f
John A. Fedoruk <johnny@johnfedoruk.ca>
2018-04-01 21:12:21 -0500

    Linting

--- src/injectable.ts
+++ src/injectable.ts
@@ -1,6 +1,6 @@
-import { GenericClassDecorator } from './generic-class-decorator';
-import { Type } from './type';
-import { Injector } from './injector';
+import { GenericClassDecorator } from "./generic-class-decorator";
+import { Injector } from "./injector";
+import { Type } from "./type";
 
 export const Injectable = (): GenericClassDecorator<Type<object>> => {
     return (target: Type<object>) => {
--- src/injector.ts
+++ src/injector.ts
@@ -1,5 +1,5 @@
-import 'reflect-metadata';
-import { Type } from './type';
+import "reflect-metadata";
+import { Type } from "./type";
 
 export namespace Injector {
 	const INSTANCE_TABLE: Map<string, any> = new Map<string, any>();
@@ -8,20 +8,20 @@ export namespace Injector {
 
 	// resolving instances
 	export const resolve = <T>(target: Type<any>): T  => {
-		let injectable: T = INSTANCE_TABLE.get((<any>target).name);
+		let injectable: T = INSTANCE_TABLE.get((target as any).name);
 		if (injectable !== undefined) {
 			return injectable;
 		}
 		// tokens are required dependencies, while injections are resolved tokens from the Injector
-		const tokens = Reflect.getMetadata('design:paramtypes', target) || [];
+		const tokens = Reflect.getMetadata("design:paramtypes", target) || [];
 		const injections = tokens.map((token: any) => Injector.resolve<any>(token));
 		injectable = new target(...injections);
-		INSTANCE_TABLE.set((<any>target).name, injectable);
+		INSTANCE_TABLE.set((target as any).name, injectable);
 		return injectable;
 	};
 
 	// store injectables within the injector
 	export const set = (target: Type<any>): void => {
-		injectables.set((<any>target).name, target);
+		injectables.set((target as any).name, target);
 	};
 }
