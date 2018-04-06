75a5da70e318d5e2cf311ad807e7c512d8a8e124
John A. Fedoruk <johnny@johnfedoruk.ca>
2018-04-06 07:39:26 -0500

    Linting

--- src/resolve.ts
+++ src/resolve.ts
@@ -1,8 +1,8 @@
+import { Injector } from "./injector";
 import { Type } from "./type";
-import { Injector } from './injector';
 
 export function Resolve(injectable: Type<any>): any {
     return (target: any, propertyKey: string): any => {
         target[propertyKey] = Injector.resolve(injectable);
-    }
+    };
 }
